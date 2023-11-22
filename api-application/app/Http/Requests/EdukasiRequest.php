03e6fcf2a5cd96c76c15369f700e7e92530414d8
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-11-22 14:06:01 +0700

    feat: menambahkan jalur request edukasi

---
+++ api-application/app/Http/Requests/EdukasiRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EdukasiRequest extends CoreRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        switch ($this->getMethod()) {
+            case 'POST': // Untuk membuat (store)
+                return [
+                    "id_admin" => "required|integer",
+                    "judul" => "required|string",
+                    "materi" => "nullable",
+                    "gambar" => "nullable",
+                ];
+            case 'PUT': // Untuk membuat (store)
+                return [
+                    "id_berita" => "required|integer",
+                    "judul" => "required|string",
+                    "materi" => "nullable",
+                    "gambar" => "nullable",
+                ];
+            case 'DELETE':
+                return [
+                    "id_berita" => "required|integer",
+                ];
+            case 'GET':
+                return [
+                    'id_berita' => 'required|integer',
+                    'search' => 'nullable'
+                ];
+            default:
+                return [];
+        }
+    }
+    public function messages(): array
+    {
+        return [
+            'id_programmers.integer' => 'id_programmer harus berupa angka',
+            'id_projects.integer' => 'id_projects harus berupa angka',
+        ];
+    }
+}
