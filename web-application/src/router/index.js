20146f04d4e38d52e3acbff16a459039dc27e601
haidar halwi <haidarhalwi@gmail.com>
2023-12-12 12:50:39 +0700

    merapihkan tampilan

--- web-application/src/router/index.js
+++ web-application/src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import config from '@/@core/config.vue'
-import axios from 'axios'
+import { createRouter, createWebHistory } from "vue-router";
+import config from "@/@core/config.vue";
+import axios from "axios";
 
 const isAuthenticated = async () => {
   const url = `${config.urlServer}/api/auth`;
@@ -48,99 +48,108 @@ const requireGuest = async (to, from, next) => {
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', redirect: '/dashboard' },
     {
-      path: '/',
-      beforeEnter:requireAuth,
-      component: () => import('../layouts/default.vue'),
+      path: "/",
+      component: () => import("../layouts/guest-layout.vue"),
       children: [
         {
-          path: 'dashboard',
-          component: () => import('../pages/dashboard.vue'),
-        },
-        {
-          path: 'account-settings',
-          component: () => import('../pages/account-settings.vue'),
-        },
-        {
-          path: 'berita-acara',
-          component: () => import('../pages/berita-acara.vue'),
-        },
-        {
-          path: 'edukasi',
-          component: () => import('../pages/edukasi.vue'),
-        },
-        {
-          path: 'galeri',
-          component: () => import('../pages/galeri.vue'),
-        },
-        {
-          path: 'data/',
-          children: [
-              {
-                path: 'format-1',
-                component: () => import('../pages/format-1.vue'),
-              },
-              {
-                path: 'format-1-create',
-                component: () => import('../pages/format-1-create.vue'),
-              },
-              {
-                path: 'format-2',
-                component: () => import('../pages/format-2.vue'),
-              },
-              {
-                path: 'format-3',
-                component: () => import('../pages/format-3.vue'),
-              },
-              {
-                path: 'format-4',
-                component: () => import('../pages/format-4.vue'),
-              },
-              {
-                path: 'format-5',
-                component: () => import('../pages/format-5.vue'),
-              },
-              {
-                path: 'format-6',
-                component: () => import('../pages/format-6.vue'),
-              },
-              {
-                path: 'format-7',
-                component: () => import('../pages/format-7.vue'),
-              },
-          ]
-        },
-        {
-          path: 'tentang',
-          component: () => import('../pages/tentang.vue'),
-        },
-        {
-          path: 'form-layouts',
-          component: () => import('../pages/form-layouts.vue'),
+          path: "",
+          component: () => import("../pages/guest.vue"),
         },
       ],
     },
     {
-      path: '/',
-      beforeEnter:requireGuest,
-      component: () => import('../layouts/blank.vue'),
+      path: "/",
+      beforeEnter: requireAuth,
+      component: () => import("../layouts/default.vue"),
       children: [
         {
-          path: 'login',
-          component: () => import('../pages/login.vue'),
+          path: "dashboard",
+          component: () => import("../pages/dashboard.vue"),
         },
         {
-          path: 'register',
-          component: () => import('../pages/register.vue'),
+          path: "account-settings",
+          component: () => import("../pages/account-settings.vue"),
         },
         {
-          path: '/:pathMatch(.*)*',
-          component: () => import('../pages/[...all].vue'),
+          path: "berita-acara",
+          component: () => import("../pages/berita-acara.vue"),
+        },
+        {
+          path: "edukasi",
+          component: () => import("../pages/edukasi.vue"),
+        },
+        {
+          path: "galeri",
+          component: () => import("../pages/galeri.vue"),
+        },
+        {
+          path: "data/",
+          children: [
+            {
+              path: "format-1",
+              component: () => import("../pages/format-1.vue"),
+            },
+            {
+              path: "format-1-create",
+              component: () => import("../pages/format-1-create.vue"),
+            },
+            {
+              path: "format-2",
+              component: () => import("../pages/format-2.vue"),
+            },
+            {
+              path: "format-3",
+              component: () => import("../pages/format-3.vue"),
+            },
+            {
+              path: "format-4",
+              component: () => import("../pages/format-4.vue"),
+            },
+            {
+              path: "format-5",
+              component: () => import("../pages/format-5.vue"),
+            },
+            {
+              path: "format-6",
+              component: () => import("../pages/format-6.vue"),
+            },
+            {
+              path: "format-7",
+              component: () => import("../pages/format-7.vue"),
+            },
+          ],
+        },
+        {
+          path: "tentang",
+          component: () => import("../pages/tentang.vue"),
+        },
+        {
+          path: "form-layouts",
+          component: () => import("../pages/form-layouts.vue"),
+        },
+      ],
+    },
+    {
+      path: "/",
+      beforeEnter: requireGuest,
+      component: () => import("../layouts/blank.vue"),
+      children: [
+        {
+          path: "login",
+          component: () => import("../pages/login.vue"),
+        },
+        {
+          path: "register",
+          component: () => import("../pages/register.vue"),
+        },
+        {
+          path: "/:pathMatch(.*)*",
+          component: () => import("../pages/[...all].vue"),
         },
       ],
     },
   ],
-})
+});
 
-export default router
+export default router;
