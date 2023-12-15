5fea17bb2946f03a5f0e01343a9b506b7b094adf
haidar halwi <haidarhalwi@gmail.com>
2023-12-15 17:03:50 +0700

    fetch api, icon dan card

---
+++ web-application/src/pages/card-galeri-guest.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="text-center" style="margin-top: 200px">
+    <strong style="font-size: 35px">GALERI</strong>
+  </div>
+
+  <VRow>
+    <VCol>
+      <div class="d-flex flex-wrap gap-2 float-right">
+        <!-- <VBtn id="gambar" color="primary" @click="inputGambar">
+          <VIcon icon="bx-cloud-upload" class="d-sm-none" />
+          <span class="d-none d-sm-block">Upload new photo</span>
+        </VBtn> -->
+
+        <input
+          id="inputGambar"
+          ref="refInputEl"
+          type="file"
+          name="file"
+          accept=".jpeg,.png,.jpg"
+          hidden
+          @change="changeAvatar"
+        />
+      </div>
+    </VCol>
+  </VRow>
+  <VRow>
+    <VCol cols="12" md="3" sm="6" v-for="data in dataGambar">
+      <VImg
+        style="width: 100%; height: 200px"
+        cover
+        :src="data.gambar"
+        alt="image"
+        @click="lihatGambar(data.gambar, data.nama_lengkap, data.id_gambar)"
+      />
+      <!-- <h1>{{ data.id_gambar }}</h1> -->
+    </VCol>
+  </VRow>
+  <VRow>
+    <VCol>
+      <div class="text-center my-3 float-right">
+        <v-pagination
+          v-model="page"
+          :length="banyakPage"
+          :total-visible="5"
+          @click="fetchData"
+        ></v-pagination>
+      </div>
+    </VCol>
+  </VRow>
+</template>
+
+<script>
+//import EditEdukasi from "./EditEdukasi.vue";
+import wallet from "@/assets/images/pages/1.png";
+import axios from "axios";
+import config from "@/@core/config.vue";
+import { ref } from "vue";
+import Swal from "sweetalert2";
+
+export default {
+  data() {
+    return {
+      dialog: ref([]),
+      dataGambar: ref([]),
+      urlServer: config.urlServer,
+      refInputEl: ref(),
+      page: 1,
+      banyakPage: 0,
+      wallet,
+    };
+  },
+
+  methods: {
+    inputGambar() {
+      document.getElementById("inputGambar").click();
+    },
+
+    async fetchData() {
+      const banyakDataTampil = 12;
+      const response = await axios.get(
+        `${this.urlServer}/api/gambar?start=${this.page}&length=${banyakDataTampil}`
+      );
+      this.dataGambar = response.data.gambar.map((item) => {
+        item.gambar = ref(this.urlServer + item.gambar);
+        item.refInput = ref();
+        item.nama_lengkap = ref(item.nama_lengkap);
+        return item;
+      });
+      this.banyakPage = Math.ceil(response.data.jumlah_data / banyakDataTampil);
+    },
+
+    async lihatGambar(gambar, judul, id_gambar) {
+      const ask = await Swal.fire({
+        imageUrl: gambar,
+        html: judul,
+        imageAlt: "Ini gambar",
+        // showConfirmButton: false,
+        // showDenyButton: true,
+        // showCancelButton: true,
+        // denyButtonText: "Hapus",
+      });
+      if (ask.isDenied) {
+        const response = await axios.delete(
+          `${this.urlServer}/api/gambar?id_gambar=${id_gambar}`,
+          {
+            headers: {
+              Authorization: localStorage.getItem("tokenAuth"),
+            },
+          }
+        );
+        if (response.data.success) {
+          Swal.fire({
+            toast: true,
+            position: "top",
+            iconColor: "white",
+            color: "white",
+            background: "rgb(var(--v-theme-success))",
+            showConfirmButton: false,
+            timerProgressBar: true,
+            timer: 1500,
+            icon: "success",
+            title: response.data.success.message,
+          });
+          this.fetchData();
+        }
+      }
+    },
+
+    // async changeAvatar(file) {
+    //   const files = file.target.files[0];
+    //   if (files) {
+    //     const fileReader = new FileReader();
+    //     // Validasi tipe file sebelum menampilkan gambarnya
+    //     if (
+    //       files.type === "image/jpeg" ||
+    //       files.type === "image/png" ||
+    //       files.type === "image/jpg"
+    //     ) {
+    //       fileReader.readAsDataURL(files);
+    //       fileReader.onload = async () => {
+    //         if (typeof fileReader.result === "string") {
+    //           const response = await axios.post(
+    //             `${this.urlServer}/api/gambar`,
+    //             {
+    //               id_admin: localStorage.getItem("id_admin"),
+    //               gambar: fileReader.result,
+    //             },
+    //             {
+    //               headers: {
+    //                 Authorization: localStorage.getItem("tokenAuth"),
+    //               },
+    //             }
+    //           );
+    //           window.location.href = "/galeri";
+    //         }
+    //       };
+    //     } else {
+    //       // Tindakan jika tipe file tidak valid
+    //       alert("File harus berupa gambar dengan tipe jpeg, png, atau jpg.");
+    //       resetAvatar();
+    //     }
+    //   }
+    // },
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
--- web-application/src/pages/guest.vue
+++ web-application/src/pages/guest.vue
@@ -2,9 +2,14 @@
 import langit from "@images/pages/2.png";
 import cardEdukasi from "./card-edukasi.vue";
 import cardBeritaAcara from "./card-berita-acara.vue";
+import cardGaleri from "./card-galeri-guest.vue";
 import tentangGuest from "./tentang-guest.vue";
 import footerGuest from "./footer-guest.vue";
 import logo from "@images/logo.svg?raw";
+
+import iconTentangPosyandu from "@images/cards/icon-tentang-posyandu.png";
+import iconEdukasi from "@images/cards/icon-edukasi-guest.png";
+import iconBeritaAcara from "@images/cards/icon-berita-guest.png";
 import { ref } from "vue";
 import { onMounted } from "vue";
 import axios from "axios";
@@ -12,14 +17,29 @@ import config from "@/@core/config.vue";
 
 const urlServer = config.urlServer;
 let statEdukasi = ref(0);
+let statBerita = ref(0);
+
+const statGambar = ref(0);
 
 const fetchEdukasi = async () => {
   const response = await axios.get(`${urlServer}/api/edukasi?start=0&length=0`);
   statEdukasi.value = response.data.jumlah_data;
 };
 
+const fetchBerita = async () => {
+  const response = await axios.get(`${urlServer}/api/berita?start=0&length=0`);
+  statBerita.value = response.data.jumlah_data;
+};
+
+const fetchGambar = async () => {
+  const response = await axios.get(`${urlServer}/api/gambar?start=0&length=0`);
+  statGambar.value = response.data.jumlah_data;
+};
+
 onMounted(() => {
+  fetchBerita();
   fetchEdukasi();
+  fetchGambar();
 });
 </script>
 
@@ -49,102 +69,60 @@ onMounted(() => {
       <div class="text-center" style="margin-top: 200px">
         <strong style="font-size: 35px">KATEGORI - KATEGORI</strong>
       </div>
-      <v-col cols="12" md="12" style="max-width: 380">
-        <div class="card-tiga">
-          <v-card class="mx-auto" width="280">
-            <!-- <v-img
-            class="align-end text-white"
-            height="200"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-            cover
-          >
-          </v-img> -->
-            <center>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="224"
-                height="224"
-                viewBox="0 0 24 24"
-                style="fill: rgba(84, 104, 255, 1); transform: ; msfilter; "
-              >
-                <path
-                  d="M19 4h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm-3.648 11.711L12.002 19l-3.349-3.289a2.129 2.129 0 0 1 0-3.069 2.224 2.224 0 0 1 3.125 0l.224.219.224-.219a2.225 2.225 0 0 1 3.126 0 2.129 2.129 0 0 1 0 3.069zM19 9H5V7h14v2z"
-                ></path>
-              </svg>
-            </center>
-
-            <h2
-              class="text-center"
-              v-bind="{
-                stats: statEdukasi,
-              }"
-            >
-              <p style="margin: 0px; font-size: 20px">JUMLAH EDUKASI</p>
-            </h2>
-          </v-card>
-
-          <v-card class="mx-auto" width="280">
-            <!-- <v-img
-            class="align-end text-white"
-            height="200"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-            cover
-          >
-          </v-img> -->
-
-            <center>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="224"
-                height="224"
-                viewBox="0 0 24 24"
-                style="fill: rgba(84, 104, 255, 1); transform: scaleY(-1)"
-              >
-                <path
-                  d="M20 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm-1 4v2h-5V7h5zm-5 4h5v2h-5v-2zM4 19V5h7v14H4z"
-                ></path>
-              </svg>
-            </center>
-
-            <h2 class="text-center">
-              <p style="margin: 0px; font-size: 20px">JUMLAH BERITA & ACARA</p>
-            </h2>
-          </v-card>
-
-          <v-card class="mx-auto" width="280">
-            <!-- <v-img
-            class="align-end text-white"
-            height="200"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-            cover
-          >
-          </v-img> -->
-            <center>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="224"
-                height="224"
-                viewBox="0 0 24 24"
-                style="fill: rgba(84, 104, 255, 1); transform: ; msfilter: "
-              >
-                <path
-                  d="m12.223 11.641-.223.22-.224-.22a2.224 2.224 0 0 0-3.125 0 2.13 2.13 0 0 0 0 3.07L12 18l3.349-3.289a2.13 2.13 0 0 0 0-3.07 2.225 2.225 0 0 0-3.126 0z"
-                ></path>
-                <path
-                  d="m21.707 11.293-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707zM18.001 20H6v-9.585l6-6 6 6V15l.001 5z"
-                ></path>
-              </svg>
-            </center>
-
-            <h2 class="text-center" style="margin: 20px">
-              <strong>TENTANG POSYANDU</strong>
-            </h2>
-          </v-card>
-        </div>
-      </v-col>
     </div>
+    <VCol cols="12" sm="12">
+      <VRow>
+        <!-- 👉 Profit -->
+        <VCol cols="12" md="4">
+          <CardStatisticsVertical
+            href=""
+            v-bind="{
+              title: 'Jumlah Berita',
+              image: iconBeritaAcara,
+              stats: statBerita,
+            }"
+          />
+        </VCol>
+
+        <!-- 👉 Sales -->
+        <VCol cols="12" md="4">
+          <CardStatisticsVertical
+            href=""
+            v-bind="{
+              title: 'Jumlah Edukasi',
+              image: iconEdukasi,
+              stats: statEdukasi,
+            }"
+          />
+        </VCol>
+        <VCol cols="12" md="4">
+          <CardStatisticsVertical
+            href=""
+            v-bind="{
+              title: 'Jumlah Galeri',
+              image: iconTentangPosyandu,
+              stats: statGambar,
+            }"
+          />
+        </VCol>
+        <!-- 👉 Profit -->
+
+        <!-- 👉 Sales -->
+        <!-- <VCol cols="12" md="4">
+          <CardStatisticsVertical
+          v-bind="{
+              title: 'Jumlah Gambar',
+              image: info,
+              stats: jumlahPengurus,
+            }"
+            />
+          </VCol> -->
+      </VRow>
+    </VCol>
+
     <cardEdukasi />
     <cardBeritaAcara />
+    <cardGaleri />
     <tentangGuest />
   </div>
   <footerGuest />
@@ -159,9 +137,7 @@ onMounted(() => {
 }
 
 /* card 3 */
-.card-tiga {
-  display: flex;
-  justify-content: space-between;
-  margin-top: 50px;
+.kategori-card:hover {
+  box-shadow: #5468ff;
 }
 </style>
