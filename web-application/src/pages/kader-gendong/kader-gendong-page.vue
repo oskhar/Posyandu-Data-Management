7d1ea4489b3e685d9bb22557511695e8ce2c6c26
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-07-11 23:24:15 +0700

    refactor: remove redundant `kader-gendong-tabs` component

--- web-application/src/pages/kader-gendong/kader-gendong-page.vue
+++ web-application/src/pages/kader-gendong/kader-gendong-page.vue
@@ -1,7 +1,54 @@
 <script setup>
-import KaderGendongTabs from "./components/kader-gendong-tabs.vue"
+import { ref } from 'vue';
+import ListDraftSuratTugas from './components/draft-surat-tugas/list-draft-surat-tugas.vue';
+import BuatSuratTugas from './components/buat-surat-tugas/buat-surat-tugas.vue';
+import ListHistorySuratTugas from './components/history-surat-tugas/list-history-surat-tugas.vue';
+
+const tabs = [
+  {
+    title: "Buat Surat",
+    icon: "bx-file",
+    tab: "buat-surat",
+  },
+  {
+    title: "Draft Surat",
+    icon: "bx-folder",
+    tab: "draft-surat",
+  },
+  {
+    title: "Histori Surat",
+    icon: "bx-list-ul",
+    tab: "histori-surat",
+  },
+];
+
+const activeTab = ref(tabs[0].tab);
+
+const createSurat = surat => {
+  console.log(surat);
+}
 </script>
 
 <template>
-  <KaderGendongTabs />
+  <VTabs v-model="activeTab" show-arrows>
+    <VTab v-for="item in tabs" :key="item.icon" :value="item.tab">
+      <VIcon size="20" start :icon="item.icon" />
+      {{ item.title }}
+    </VTab>
+  </VTabs>
+  <VDivider />
+
+  <VWindow v-model="activeTab" class="mt-5 disable-tab-transition">
+    <VWindowItem value="buat-surat">
+      <BuatSuratTugas @create="createSurat" />
+    </VWindowItem>
+
+    <VWindowItem value="draft-surat">
+      <ListDraftSuratTugas />
+    </VWindowItem>
+
+    <VWindowItem value="histori-surat">
+      <ListHistorySuratTugas />
+    </VWindowItem>
+  </VWindow>
 </template>
